7b9a00a9d648a44285547cf4484c8bb4b3b3562d
Jason Chin <jason.rob.chin@gmail.com>
2017-05-01 23:05:46 -0400

    Initialize repository

---
+++ README.md
@@ -0,0 +1,2 @@
+# Cards With Friends
+A way to play cards with friends all on separate devices.
---
+++ cardswithfriends/__init__.py
@@ -0,0 +1,6 @@
+from flask import Flask
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+# Now we can access the configuration variables via app.config["VAR_NAME"].
